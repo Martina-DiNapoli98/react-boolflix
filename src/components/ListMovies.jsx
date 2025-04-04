f55f5625016b4c333e0b7b8e1930ff0dd653b5a1
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-04-04 17:58:39 +0200

    Enhance movie list display with language flags and improved formatting

--- src/components/ListMovies.jsx
+++ src/components/ListMovies.jsx
@@ -1,23 +1,36 @@
-import { useMovieContext } from "../Contexts/MovieContext"
+import { useMovieContext } from "../Contexts/MovieContext";
+import LangMap from "../data/LangMap";
 
 export default function ListMovies() {
-    const { movies } = useMovieContext()
-    return (
-        <>
-            <ul className="list">
-                {
-                    movies.map((movie) => (
-                        <li key={`Movie-${movie.id}`}>
-                            <h6>{movie.title}</h6>
-                            <h6>{movie.original_title}</h6>
-                            <h6>{movie.original_language}</h6>
-                            <h6>{movie.vote_average}</h6>
-                        </li>
-                    ))
-                }
-            </ul>
-        </>
-    )
-}
+    const { movies } = useMovieContext();
 
+    return (
+        <ul className="list">
+            {movies.map((movie) => {
+                const lang = movie.original_language;
+                const countryCode = LangMap[lang];
+                return (
+                    <li key={`Movie-${movie.id}`}>
+                        <p><strong>Titolo:</strong> {movie.title}</p>
+                        <p><strong>Titolo originale:</strong> {movie.original_title}</p>
+                        <p>
+                            <strong>Lingua:</strong>{" "}
+                            {countryCode ? (
+                                <img
+                                    src={`https://flagcdn.com/w40/${countryCode}.png`}
+                                    alt={lang}
+                                    width="25"
+                                    style={{ verticalAlign: "middle" }}
+                                />
+                            ) : (
+                                <span>{lang}</span>
+                            )}
+                        </p>
+                        <p><strong>Voto:</strong> {movie.vote_average}</p>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+}
 
---
+++ src/data/LangMap.jsx
@@ -0,0 +1,14 @@
+const LangMap = {
+    en: "gb",
+    it: "it",
+    fr: "fr",
+    es: "es",
+    de: "de",
+    ja: "jp",
+    ko: "kr",
+    zh: "cn",
+    hi: "in",
+    ru: "ru",
+};
+
+export default LangMap;
